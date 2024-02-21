720fddb92020bfca2df160afa7ac624cbc0d9e84
Jerakin <the_rensvind@hotmail.com>
2024-02-21 20:46:16 +0100

    fix: clean up tests

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,36 +1,40 @@
-from wikmd import wiki
 import pytest
-
+from wikmd import wiki
 from wikmd.wiki import app
 
-@pytest.fixture
+
+@pytest.fixture()
 def client():
     return app.test_client
 
+
 def test_plugin_loading():
     assert wiki.plugins
+
 
 def test_process_md():
     before = "#test this is test\n text should still be available after plugin"
     md = before
     for plugin in wiki.plugins:
-        if ("process_md" in dir(plugin)):
+        if "process_md" in dir(plugin):
             md = plugin.process_md(md)
     assert md == before
+
 
 def test_draw_md():
     before = "#test this is test\n[[draw]] \n next line"
     md = before
     for plugin in wiki.plugins:
-        if ("process_md" in dir(plugin)):
+        if "process_md" in dir(plugin):
             md = plugin.process_md(md)
     assert md != before
     assert md != ""
-    
+
+
 def test_process_html():
     before = "<html><h1>this is a test</h1></html>"
     html = before
     for plugin in wiki.plugins:
-        if ("process_html" in dir(plugin)):
+        if "process_html" in dir(plugin):
             html = plugin.process_html(html)
     assert html == before
