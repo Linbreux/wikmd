eee8639bf3e286c3562d25759a20ba023f72be06
linbreux <29354411+Linbreux@users.noreply.github.com>
2024-08-23 00:27:45 +0200

    dev: implement navbar buttons

--- src/wikmd/static/js/dark-theme.js
+++ src/wikmd/static/js/dark-theme.js
@@ -1,18 +1,19 @@
-function toggleDarkMode() {
-  //Check if there is a value in Session Storage or Not!
-  if (sessionStorage.getItem("theme") != "") {
-    $("body").attr("data-theme", "light");
-  } else {
-    theme = sessionStorage.getItem("theme"); //Get Value of theme from session
-    $("body").attr("data-theme", theme);
-  }
-  var dataTheme = $("body").attr("data-theme");
+document.addEventListener('DOMContentLoaded', function () {
+  const themeToggle = document.getElementById('theme-toggle');
+  const currentTheme = localStorage.getItem('theme') || 'wikmd-light';
+  document.documentElement.setAttribute('data-theme', currentTheme);
 
-  if (dataTheme === "dark") {
-    $("body").attr("data-theme", "light");
-    sessionStorage.setItem("theme", "light"); // Store the value in Session Storage
-  } else {
-    sessionStorage.setItem("theme", "dark"); // Store the value in Session Storage
-    $("body").attr("data-theme", "dark");
+  if (currentTheme === 'wikmd-dark') {
+    themeToggle.checked = true;
   }
-}
+
+  themeToggle.addEventListener('change', function () {
+    if (themeToggle.checked) {
+      document.documentElement.setAttribute('data-theme', 'wikmd-dark');
+      localStorage.setItem('theme', 'wikmd-dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'wikmd-light');
+      localStorage.setItem('theme', 'wikmd-light');
+    }
+  });
+});
